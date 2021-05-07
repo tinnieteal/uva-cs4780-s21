d441abf16ac3357050a0b93e18a6d69c9564f55c
MAGGIELIU1012 <liu_chenling1@163.com>
2021-05-06 21:20:17 -0500

    push test

--- MATA/Search/models.py
+++ MATA/Search/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# Previous model
 class Item(models.Model):
     title=models.CharField(max_length=128)
     description=models.TextField()
