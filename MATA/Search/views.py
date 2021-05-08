ecd535943c3bf08bde2c2cbd3c3071799438cd16
Tina Chen <tc7nz@virginia.edu>
2021-05-08 17:49:30 -0400

    placed bm25 ranking function within ranking_utils.py

--- MATA/Search/views.py
+++ MATA/Search/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator
 from .models import *
 from .utils import *
+from .ranking_utils import *
 
 import math
 
@@ -36,36 +37,6 @@ def result(request):
     results.sort(key=lambda element: element[0], reverse=True) 
 
     return render(request, 'search/result.html',{'query': query, 'results': results})
-
-
-num_items = 1000
-average_item_length = 110.297
-k1 = 1.5 ## [1.2, 2.0]
-b = 0.75 
-
-def bm25(query, item_obj): 
-    score = 0
-    item_length = item_obj.desc_length + item_obj.title_length + item_obj.review_length
-
-    for token in nltk_process(query):
-        indices = Index.objects.filter(word=token).all()
-        num_doc = 0
-        total_freq = 0
-
-        if len(indices) != 0: 
-            num_doc = indices.first().items.count()
-
-            mems = Membership.objects.filter(index=indices.first(), item=item_obj).all()
-            if len(mems) != 0: 
-                mem = mems.first()
-                total_freq = mem.des_df + mem.title_df + mem.review_df
-
-        score += idf(num_doc) * ((total_freq * (k1+1)) / (total_freq + k1 * (1-b+b * item_length /average_item_length)))
-
-    return score
-
-def idf(num_doc):
-    return math.log( (num_items - num_doc + 0.5) / (num_doc + 0.5) + 1 )
 
     # # loop through the tokenized & normalized token of the query
     # for token in nltk_process(query):
