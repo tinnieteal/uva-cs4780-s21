03a8668c3c170fb225eca50f8b409d8f728af5d4
zw3hk <70671608+zw3hk@users.noreply.github.com>
2021-05-12 18:16:57 -0400

    highlight fix

--- MATA/Search/templatetags/my_tags.py
+++ MATA/Search/templatetags/my_tags.py
@@ -5,14 +5,24 @@ from django.template import Template
 from django.utils.safestring import mark_safe
 from re import IGNORECASE, compile, escape as rescape
 
+import nltk
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+
+#nltk.download("punkt")
+#nltk.download('averaged_perceptron_tagger')
+
 register = template.Library()
 
 @register.filter
 def highlight_search(text, search):
-    rgx = compile(rescape(search), IGNORECASE)
-    return mark_safe(
-        rgx.sub(
-            lambda m: '<mark>{}</mark>'.format(m.group()),
-            text
-        )
-    )
+    tokens = nltk.word_tokenize(search)
+    i = 0
+    for token in tokens:
+        rgx = compile(rescape(token), IGNORECASE)
+        text = rgx.sub(
+                lambda m: '<mark>{}</mark>'.format(m.group()),
+                text
+            )
+    return mark_safe(text)
