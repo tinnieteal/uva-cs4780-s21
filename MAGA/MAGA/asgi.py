3d31ce53accd8a3bb704877fd648be7cc11e4bfc
Tina Chen <tc7nz@virginia.edu>
2021-05-20 23:16:52 -0400

    updated MAGA setting

--- MAGA/MAGA/asgi.py
+++ MAGA/MAGA/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MATA project.
+ASGI config for MAGA project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MATA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MAGA.settings')
 
 application = get_asgi_application()
